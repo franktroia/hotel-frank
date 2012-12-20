05caad423317c374b741b04e5300948f92928722
Frank Troia <frank.troia@gmail.com>
2012-12-20 16:27:13 -0600

    Rails project creation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelFrank::Application.config.secret_token = '4d3c4d19735072e5031bfe988bb3a1f305eda3cd19971ad99b1b700674e6641c95dcedbe9cd00d43fe9e1187f7b1a26866332186d8c4868af12cae4f9ea443fd'
